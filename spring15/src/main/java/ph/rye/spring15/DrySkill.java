0fa32c1057f3f1b904f298b5720a91e33d25d2ea
Royce Remulla <royce.com@gmail.com>
2015-12-29 15:12:56 +0800

    Dependency Injection List Property Beans using XML and util Namespace

---
+++ spring15/src/main/java/ph/rye/spring15/DrySkill.java
@@ -0,0 +1,20 @@
+package ph.rye.spring15;
+
+import java.io.PrintStream;
+
+public class DrySkill implements Skill {
+
+
+    private final PrintStream stream;
+
+
+    public DrySkill(final PrintStream stream) {
+        this.stream = stream;
+    }
+
+    @Override
+    public void apply() {
+        stream.println("Applying dry skill...");
+    }
+
+}
---
+++ spring15/src/main/java/ph/rye/spring15/Skill.java
@@ -0,0 +1,7 @@
+package ph.rye.spring15;
+
+public interface Skill {
+
+    void apply();
+
+}
