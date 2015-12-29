456ac0c49b4849213a0dee12538eb3f78e14d2bd
Royce Remulla <royce.com@gmail.com>
2015-12-29 15:02:33 +0800

    Dependency Injection List Property Values using XML

---
+++ spring13/src/main/java/ph/rye/spring13/App.java
@@ -0,0 +1,41 @@
+/**
+ *   Copyright 2015 Royce Remulla
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package ph.rye.spring13;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/** */
+public class App {
+
+
+    final AbstractApplicationContext appContext =
+            new ClassPathXmlApplicationContext("/META-INF/spring/spring.xml");
+
+    public Developer getBean() {
+        return appContext.getBean(Developer.class);
+    }
+
+
+    public static void main(final String... args) {
+
+        final App app = new App();
+        app.getBean().performDuty();
+
+        app.appContext.close();
+    }
+
+}
---
+++ spring13/src/main/java/ph/rye/spring13/RealDeveloper.java
@@ -0,0 +1,81 @@
+/**
+ *   Copyright 2015 Royce Remulla
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package ph.rye.spring13;
+
+import java.io.PrintStream;
+import java.util.List;
+
+/**
+ * @author royce
+ *
+ */
+public class RealDeveloper implements Developer {
+
+
+    private final Duty duty;
+
+    private final PrintStream stream;
+
+    private Skill skill;
+
+    private List<String> contacts;
+
+
+    public RealDeveloper(final Duty duty, final PrintStream stream) {
+        this.duty = duty;
+        this.stream = stream;
+    }
+
+    /* (non-Javadoc)
+     * @see ph.rye.spring05.Developer#performTask()
+     */
+    @Override
+    public void performDuty() {
+        if (skill != null) {
+            skill.apply();
+        }
+        if (contacts != null) {
+            stream.println(
+                "If need be, I have these contact numbers: " + contacts);
+        }
+
+        duty.perform();
+    }
+
+    /** Test Seam */
+    Duty getDuty() {
+        return duty;
+    }
+
+
+    /** Test Seam */
+    Skill getSkill() {
+        return skill;
+    }
+
+    public void setSkill(final Skill skill) {
+        this.skill = skill;
+    }
+
+    List<String> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(final List<String> contacts) {
+        this.contacts = contacts;
+    }
+
+}
