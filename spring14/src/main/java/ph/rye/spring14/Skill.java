9991aa904bbdbbd14dd8538ef33c50af930df78b
Royce Remulla <royce.com@gmail.com>
2015-12-29 15:12:22 +0800

    Dependency Injection List Property Beans using XML

---
+++ spring14/src/main/java/ph/rye/spring14/Skill.java
@@ -0,0 +1,7 @@
+package ph.rye.spring14;
+
+public interface Skill {
+
+    void apply();
+
+}
---
+++ spring14/src/test/java/ph/rye/spring14/AppTest.java
@@ -0,0 +1,43 @@
+/**
+ *   Copyright 2015 Royce Remulla
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package ph.rye.spring14;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * @author royce
+ */
+public class AppTest {
+
+
+    private final App sut = new App();
+
+
+    /**
+     * Test method for {@link ph.rye.spring14.App#getBean()}.
+     */
+    @Test
+    public void testBeanInstantatedAndPropertyListInjected() {
+        final RealDeveloper developerBean = (RealDeveloper) sut.getBean();
+        Assert.assertNotNull(developerBean.getDuty());
+        Assert.assertNotNull(developerBean.getSkill());
+        Assert.assertNotNull(developerBean.getContacts());
+        Assert.assertNotNull(developerBean.getTools());
+    }
+
+}
